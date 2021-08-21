a547d1e02f0096bf6f30b0404bc37c15314482cd
marjanmukram <marjanmukram8@gmail.com>
2021-08-21 15:35:19 +0530

    tuples

--- app.ts
+++ app.ts
@@ -1,19 +1,23 @@
-// Generally not recommended.
-
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: "Marjan",
-//   age: 22,
-// };
-
-// console.log(person.name);
-
 // Lets allow TS to infer types
-const person2 = {
+const person2: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Marjan",
   age: 22,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
-console.log(person2.name);
+person2.role.push("admin");
+// person2.role[1] = 2;
+// person2.role = [0, "adimin", 2];
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports"];
+
+for (const hobby of person2.hobbies) {
+  console.log(hobby.toUpperCase());
+}
