2c47edd1d00f85690a44b64245502f808056749d
marjanmukram <marjanmukram8@gmail.com>
2021-08-21 17:48:56 +0530

    function types

--- app.ts
+++ app.ts
@@ -1,18 +1,18 @@
+// fuunctiopn types
+
 function add(n1: number, n2: number): number {
   return n1 + n2;
 }
 
-function printResult(num: number): undefined {
-  console.log("Result: " + num);
-  return;
-}
-
-function printResult2(num: number): void {
+function printResult(num: number) {
   console.log("Result: " + num);
 }
 
-function printResult3(num: number) {
-  console.log("Result: " + num);
-}
+// let combineValues;
+// let combineValues: Function;
+let combineValues: (n1: number, n2: number) => number;
 
-printResult(add(5, 12));
+combineValues = add;
+// combineValues = printResult;
+
+printResult(combineValues(5, 12));
