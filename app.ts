80911767f56aede4feeabe9b387bbeed1cf4a396
marjanmukram <marjanmukram8@gmail.com>
2021-08-21 15:17:05 +0530

    object types

--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+// Generally not recommended.
 
-const n1 = 2;
-const n2 = 3;
-const printResult = true;
-const resultPhrase = "Result is : ";
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Marjan",
+//   age: 22,
+// };
 
-const result = add(n1, n2, printResult, resultPhrase);
-console.log(result);
+// console.log(person.name);
+
+// Lets allow TS to infer types
+const person2 = {
+  name: "Marjan",
+  age: 22,
+};
+
+console.log(person2.name);
